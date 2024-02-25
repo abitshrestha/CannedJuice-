36d56d4639e8367e547680fa5f0ee0c779684921
abitshrestha <abitshrestha2080@gmail.com>
2024-02-25 15:24:50 +0545

    vfsv

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthProvider>
     <CartProvider>
-      <BrowserRouter basename="https://canned-juice.vercel.app">
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </CartProvider>
